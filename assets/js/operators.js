b1f3449410cc6c893fb374b045468823af712efc
NorahCatherine <norahcatherinenansubuga@gmail.com>
2022-03-23 12:08:48 +0300

    js operators

---
+++ assets/js/operators.js
@@ -0,0 +1,38 @@
+//commonly used operatots used in js.
+//assignment operator.
+let cureentAge=22;
+let ageIn5Yrs=(cureentAge+5);//option 1
+ageIn5Yrs+= cureentAge+5;//+= means get the value of age in 5 and add something
+
+let age=0;
+//age+=5 same as below
+age=age+5;
+
+let ageInHalf= ageIn5Yrs/2
+let num=2;
+num/=ageInHalf;
+console.log('num>>>',num)
+
+let age2YrsAgo=(num-2);
+let truth=true&&true;// logical and
+let lie= true && false;
+let lie2= lie || truth;//logical or
+let lieIsh= truth && lie;
+//comparison operators
+
+let num1, num2;// camelcase
+let num_1,num_2;//snake case
+num1=55;
+num2=70;
+let isEqual=(num1==num2);
+let isGreater=(num2>num1);
+let isGreaterOrEqual=(num1>=num2);
+let isLess=(num2<num1);
+let isLessOrEqual=(num1<=num2);
+
+//DATA TYPES
+// divided into primitive types and objects
+
+
+
+
